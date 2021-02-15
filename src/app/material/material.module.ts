41c6d4ba70dea337f8910543f2e243e9376c472b
Omar Alanizi <omar-anizi@hotmail.com>
2021-02-15 21:00:59 +0300

    imports

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,6 +8,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {A11yModule} from '@angular/cdk/a11y';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
 // import {ClipboardModule} from '@angular/cdk/clipboard';
 // import {DragDropModule} from '@angular/cdk/drag-drop';
 // import {PortalModule} from '@angular/cdk/portal';
@@ -33,10 +35,8 @@ import {A11yModule} from '@angular/cdk/a11y';
 // import {MatProgressBarModule} from '@angular/material/progress-bar';
 // import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 // import {MatRadioModule} from '@angular/material/radio';
-// import {MatSelectModule} from '@angular/material/select';
 // import {MatSidenavModule} from '@angular/material/sidenav';
 // import {MatSliderModule} from '@angular/material/slider';
-// import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 // import {MatSortModule} from '@angular/material/sort';
 // import {MatTableModule} from '@angular/material/table';
 // import {MatTabsModule} from '@angular/material/tabs';
@@ -56,6 +56,8 @@ const MaterialComponents = [
   A11yModule,
   MatCardModule,
   MatIconModule,
+  MatSlideToggleModule,
+  MatSelectModule,
   // ClipboardModule,
   // CdkStepperModule,
   // CdkTableModule,
@@ -83,10 +85,8 @@ const MaterialComponents = [
   // MatProgressSpinnerModule,
   // MatRadioModule,
   // MatRippleModule,
-  // MatSelectModule,
   // MatSidenavModule,
   // MatSliderModule,
-  // MatSlideToggleModule,
   // MatSnackBarModule,
   // MatSortModule,
   // MatTableModule,
