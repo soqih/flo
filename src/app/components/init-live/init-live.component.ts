28cb90a424a833885ee5bae85b49cf443077446b
Omar Alanizi <omar-anizi@hotmail.com>
2021-02-17 13:36:04 +0300

    checkbox

--- src/app/components/init-live/init-live.component.ts
+++ src/app/components/init-live/init-live.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import {FormControl, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 interface User{
-  selected: boolean ;
+  selected: boolean;
   value: string ;
   viewValue: string ;
 }
@@ -15,53 +15,63 @@ interface User{
 })
 export class InitLiveComponent implements OnInit {
 
-  isPrivate = true;
+  // isPrivate = true;
 
-  allInvited = false;
+  // allInvited = false;
 
-  titleField = new FormControl('', [
-    Validators.required
-  ]);
+  // titleField = new FormControl('', [
+  //   Validators.required
+  // ]);
 
-  userform = new FormControl();
+  // userform = new FormControl();
+  myForm: FormGroup;
 
  // usersList: string[] = ['User1', 'User2', 'User3', 'User4'];
 
-  usersList: User[] = [
-    {selected: false , value: 'user-1' , viewValue: "user1"},
-    {selected: true , value: 'user-2' , viewValue: "user2"},
-    {selected: false , value: 'user-3' , viewValue: "user3"}
+  usersList: Array<User> = [
+    { selected: false, value: 'user-1' , viewValue: "user1"},
+    { selected: false, value: 'user-2' , viewValue: "user2"},
+    { selected: false, value: 'user-3' , viewValue: "user3"},
   ]
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   //  console.log(this.userform.value)
-    
+    this.myForm = this.fb.group({
+      title: "",
+      private: false,
+      allowedFollowers: new FormArray([]),
+      saveStream: false,
+    })
+    // this.myForm.patchValue({allowedFollowers: this.usersList})
+    this.myForm.valueChanges.subscribe(console.log);
   }
-
-  setUser(user){
-    this.allInvited = false;
-    // this.userform.
-    user.selected = !user.selected;
-  }
-
-  onClear() {
-    for (let user of this.usersList) {
-      user.selected = false;
-    }
-  }
-
-  allInvitedFunction(){
-    if (this.allInvited){
-      this.userform.setValue([])
-    } else {
-      let arr = ["All"]
-      for (let user of this.usersList) {
-        arr.push(user.value)
+  
+  onCheckChange(event) {
+    // console.log(this.myForm.get("allowedFollowers"))
+    if (event.isUserInput){
+      console.log(event);
+      const formArray: FormArray = this.myForm.get('allowedFollowers') as FormArray;
+      
+      // check selected
+      if (event.source.selected){
+        formArray.push(new FormControl(event.source.value));
       }
-      this.userform.setValue(arr);
+  
+      // check unselected
+      else {
+        // find the unselected element
+        let i: number = 0;
+        formArray.controls.forEach((ctrl: FormControl) => {
+          if (ctrl.value == event.source.value) {
+            formArray.removeAt(i);
+            return;
+          }
+          i+=1;
+        })
+      }
+
     }
-    this.allInvited = !this.allInvited;
   }
 }
