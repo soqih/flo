4c27a8f2dcec0b2aee3d7a7e1fa1e1fe1a7ea3e4
abdullah ibrahim <abo.gba@gmail.com>
2021-02-18 17:11:57 +0300

    some styling, add stream to home

--- src/app/components/stream-card/stream-card.component.ts
+++ src/app/components/stream-card/stream-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-stream-card',
@@ -15,8 +15,44 @@ export class StreamCardComponent implements OnInit {
   @Input() likes: number;
   @Input() dislikes: number;
 
+  liked: boolean = false;
+  disliked: boolean = false;
+
+  likeState: string = "thumb_up_off_alt"
+  dislikeState: string = "thumb_down_off_alt"
+
+  @ViewChild("likeIcon") likeIcon: ElementRef;
   constructor() { }
   ngOnInit(): void {
+  }
+  like() {
+    if (this.liked) {
+      this.liked = false;
+      this.likeState = "thumb_up_off_alt";
+      return;
+    }
+    if (this.disliked) {
+      this.disliked = false;
+      this.dislikeState = "thumb_down_off_alt";
+
+    }
+    this.liked = true;
+    this.likeState = "thumb_up"
+  }
+
+  dislike() {
+    if (this.disliked) {
+      this.disliked = false;
+      this.dislikeState = "thumb_down_off_alt";
+      return
+    }
+    if (this.liked) {
+      this.liked = false;
+      this.likeState = "thumb_up_off_alt";
+
+    }
+    this.dislikeState = "thumb_down";
+    this.disliked = true;
   }
 }
 
