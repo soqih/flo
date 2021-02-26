20cb6cdbf773cf6dbf8118d88f96a43012eb9353
abdullah ibrahim <58809534+soqih@users.noreply.github.com>
2021-02-26 16:32:50 +0300

    Add files via upload

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,8 +15,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isLoggedIn !== true) {
-      // this.router.navigate([''])
-      return false;
+      this.router.navigate([''])
     }
     return true;
   }
